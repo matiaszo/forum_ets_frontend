f2fe93bfea61ed9e301e38c7171c1db6cb053952
matiaszo <tiaszuniga@gmail.com>
2024-12-03 11:53:02 -0300

    just finish the justify part

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,32 @@
 import { ROUTES } from "@/constants/routes";
 import Link from "next/link";
+import Image from "next/image";
+import ets_logo from "@/assets/ets_logo.png"
+import user_logo from "@/assets/user_logo.png"
+
 
 
 export const Header = () => {
+
+    const style= {
+        option: "font-robFont text-blue5 text-xl"
+    }
     return(
         <>
-            <h1>
-                <Link className="options" href={ROUTES.chat}>Home</Link>
+        <div className="flex justify-between items-center">
+
+            <h1 className="">
+                <Image src={ets_logo} width={100} height={200} alt="ets logo"/>
             </h1>
+
+            <div className="flex mr-96 gap-8 justify-center items-center">
+                <Link className={style.option} href={ROUTES.home}>Home</Link>
+                <Link className={style.option} href={ROUTES.forum}>Fórum</Link>
+                <Link className={style.option} href={ROUTES.chat}>Chats</Link>
+                <Link className={style.option} href={ROUTES.project}>Projetos</Link>
+                <Link  href={ROUTES.chat}><Image src={user_logo} width={50} height={50} alt="ets logo"/></Link>
+            </div>
+        </div>
         </>
     )
 }
