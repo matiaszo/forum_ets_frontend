cf681ad1761361505b500f8b5e0c162a72cc3b5a
matiaszo <tiaszuniga@gmail.com>
2024-12-17 09:57:15 -0300

    some

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,6 +47,7 @@ export default function Home() {
       // Redirecionar ou salvar o token
       localStorage.setItem("token", data.token); // Exemplo de salvamento
       localStorage.setItem("id", data.id.toString()); // Exemplo de salvamento
+      localStorage.setItem("profile", data.id.toString()); // Exemplo de salvamento
 
 
       fetch(`http://localhost:8080/profile/${data.id}`,
